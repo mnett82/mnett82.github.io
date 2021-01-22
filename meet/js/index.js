b91cdbf01daa3bc7fe829202ec190d5d6387edc0
Michael Nett <mnett82@gmail.com>
2021-01-22 22:16:45 +0900

    Attempt to deploy via wasm-pack/webpack toolchain.

--- meet/js/index.js
+++
@@ -1,6 +0,0 @@
-// WASM dependencies must be loaded and compiled asynchronously. This shim takes care of the
-// asynchronous import, so that the rest of the application (see `app.js`) does not have to worry
-// about bootstrapping details.
-import("./app.js").catch(e => console.error("Error bootstrapping `app.js`:", e));
-
-  
